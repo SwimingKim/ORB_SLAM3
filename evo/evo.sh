2d3f2eae66fe6fcd1b5edeb11f648e80637149b3
skim <rumex13@naver.com>
2023-07-26 22:41:45 +0900

    cam0 cam1

--- evo/evo.sh
+++ evo/evo.sh
@@ -1,1 +1,7 @@
-evo_traj kitti CameraTrajectory.txt --align --correct_scale --ref=evo/00_mono.txt -p --plot_mode=xz
+# evo_traj kitti CameraTrajectory.txt --align --correct_scale --ref=evo/00_mono.txt -p --plot_mode=xz
+
+# evo_traj kitti CameraTrajectory.txt --correct_scale --ref=evo/00_mono.txt -p --plot_mode=xz --transform_left evo/left.json
+
+# evo_rpe kitti evo/00_mono.txt CameraTrajectory.txt --plot --plot_mode xz  --align --correct_scale --pose_relation angle_deg --delta 90 --delta_unit d --align_origin
+
+evo_rpe kitti evo/00_mono.txt CameraTrajectory.txt --plot --plot_mode xz  --align --correct_scale
