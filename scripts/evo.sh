e43cabf2ee549166fa2ab08739c723841b8ac1c7
skim <rumex13@naver.com>
2023-07-28 14:52:34 +0900

    add ros evo

--- scripts/evo.sh
+++ scripts/evo.sh
@@ -6,14 +6,16 @@
 
 # evo_rpe kitti evo/00_mono.txt CameraTrajectory.txt --plot --plot_mode xz  --align --correct_scale --pose_relation angle_deg --delta 90 --delta_unit d --align_origin
 
-line=$(cat CameraTrajectory.txt | wc -l)
-echo $line
+# line=$(cat CameraTrajectory.txt | wc -l)
+# echo $line
 
-if [ $line -eq 4541 ]
-then
-    evo_traj kitti CameraTrajectory.txt --align --correct_scale --ref=evo/gt_streo.txt -p --plot_mode=xz
-    evo_ape kitti evo/gt_streo.txt CameraTrajectory.txt --plot --plot_mode xz  --align --correct_scale
-else
-    evo_traj kitti CameraTrajectory.txt --align --correct_scale --ref=evo/gt_mono.txt -p --plot_mode=xz
-    evo_ape kitti evo/gt_mono.txt CameraTrajectory.txt --plot --plot_mode xz  --align --correct_scale
-fi
+# if [ $line -eq 4541 ]
+# then
+#     evo_traj kitti CameraTrajectory.txt --align --correct_scale --ref=evo/gt_streo.txt -p --plot_mode=xz
+#     evo_ape kitti evo/gt_streo.txt CameraTrajectory.txt --plot --plot_mode xz  --align --correct_scale
+# else
+#     evo_traj kitti CameraTrajectory.txt --align --correct_scale --ref=evo/gt_mono.txt -p --plot_mode=xz
+#     evo_ape kitti evo/gt_mono.txt CameraTrajectory.txt --plot --plot_mode xz  --align --correct_scale
+# fi
+
+evo_traj kitti CameraTrajectory_ros.txt --align --correct_scale --ref=evo/gt_ros.txt -p --plot_mode=xz
